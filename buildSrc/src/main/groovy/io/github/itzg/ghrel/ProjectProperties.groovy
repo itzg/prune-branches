f30df2d0161ee71c497c23ec0325cced8c5d4864
Geoff Bourne <itzgeoff@gmail.com>
2022-10-23 12:30:30 -0500

    Use published github-releaser plugin

--- buildSrc/src/main/groovy/io/github/itzg/ghrel/ProjectProperties.groovy
+++
@@ -1,9 +0,0 @@
-package io.github.itzg.ghrel
-
-import org.gradle.api.provider.Property
-
-abstract class ProjectProperties {
-    abstract Property<String> getLicense()
-
-    abstract Property<String> getHomepage()
-}
